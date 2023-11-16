1fe7a4f816773cced4b50f5ca6447c9aed58d30f
zedaenious <zed.dev.22@gmail.com>
2023-11-16 10:43:47 -0500

    first commit

---
+++ weather/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather
+//
+//  Created by Matt Zelenak on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather/weatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherApp.swift
+//  weather
+//
+//  Created by Matt Zelenak on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct weatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
